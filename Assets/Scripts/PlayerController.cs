fa4cf0fb3cc38f7e2cb0e77bc5de01707af23094
poik1118 <79559496+poik1118@users.noreply.github.com>
2021-05-21 12:12:47 +0900

    210521

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float translation;
+    private float rotation;
+    private Animator ani;
+
+    public float moveSpeed = 10f;
+    public float rotationSpeed = 100f;
+
+    private void Start()
+    {
+        ani = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        translation = Input.GetAxis("Vertical") * moveSpeed;
+        rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+
+        transform.Translate(0, 0, translation * Time.deltaTime);
+        transform.Rotate(0, rotation * Time.deltaTime, 0);
+
+        if(translation != 0 || rotation != 0)
+        {
+            ani.SetBool("walk", true);
+        }
+        else
+        {
+            ani.SetBool("walk", false);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Obstacle")
+        {
+            ani.SetTrigger("damage");
+        }
+    }
+}
